e3903abfb3359037a02f30acbe69354b025b195b
90PuroXx <126303785+RikoTheDemonHunter@users.noreply.github.com>
2025-01-09 08:22:00 +0800

    Update IP Logger.java

--- IP Logger.java
+++ IP Logger.java
@@ -1,7 +1,7 @@
 local file {}
 
-function auto logger, = True')
-
+function auto logger.Lua
+  Local port =14.000
   Ip grabber Dns Port = 192.168.73.63 
 
   Local port ("Name:D Link 5G")
@@ -16,9 +16,58 @@ Function = Localfile ("Delete Server")
                end 
             end)
 
-1LocalFile = loadstring(game:HttpGet("https://raw.githubusercontent.com/RikoTheDemonHunter/V3/refs/heads/main/Auto%20Equip.lua"))()
+1 LocalFile = loadstring(game:HttpGet("https://raw.githubusercontent.com/RikoTheDemonHunter/V3/refs/heads/main/Auto%20Equip.lua"))()
+
+  Ip Port = Auto request  ("Local Dns Port")
+  Return  function 9e9 ("Name:Mobile Port Host"
+                      Return function = Gateway Request
+  Function Auto Log ("Local File")
+  https://LocalPort.Lua.content.File.request
+Return Sender = File Request Gateway Port("10.40.21.192.0")
+Return Port to = 192.168.73.63 = Fake Ip ("10.23.50.0.19")
+End 
+   end
+      end
+         end
+            end
+           end
+        end 
+     end
+  end)
   
+  Return End = LocalPort Map = Return Sender ("2.2.2.2.2")
+
+Local Delete Function  do wait (3)
+  Local Delete function'
+  Local delete  function Do wait (1)
+  Manipulate WiFi Protocol  to ("main Port")
+  Main Port = ("System File Checker")
+  Second Port = ("D-Link 5G")
+
+  Local Host Return = File
+end
+   end 
+      end 
+          end 
+              end 
+                  end 
+                 end
+                end 
+               end)
+  Local File: Loadstring:HttpGet("Https//LocalPort.Lua.content.File.request
+
+Initiate  Deletion  Do wait (1.2)
+  Local Message ("This Server is permanently  Deleted On Our Server")
+  Do wait (9e9)
+
+  Return Function"()
+  end 
+      end 
+          end
+              end)
   
 
-  
 
+
+
+                        
