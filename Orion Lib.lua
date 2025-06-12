6aae50c1db2ccd9c418b142833ea02b0c15abad2
Avery <126303785+RikoTheDemonHunter@users.noreply.github.com>
2025-06-12 15:27:11 +0800

    Update Orion Lib.lua

--- Orion Lib.lua
+++ Orion Lib.lua
@@ -462,7 +462,7 @@ Farming.Farm:CreateToggle{
         Callback = function()
              loadstring(game:HttpGet("https://raw.githubusercontent.com/RikoTheDemonHunter/V3/refs/heads/main/Auto%20Equip.lua"))() 
           end
-       
+      end
 }
 
 
