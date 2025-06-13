43da8445afccb19f068e430ce48431f8a561de01
Avery <126303785+RikoTheDemonHunter@users.noreply.github.com>
2025-06-13 18:01:20 +0800

    Update Shit.lua

--- Shit.lua
+++ Shit.lua
@@ -587,7 +587,7 @@ AutoFarm:Toggle("Auto Collect Gem", function(v)
 end)
 
 AutoFarm:Toggle("Auto Drink", function(v)
-	while wait(2.444) do 
+	while wait(2.34) do 
 		game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer("Starter Drink")
 		game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer("Second Drink")
 		game.ReplicatedStorage.RemoteEvents.DrinkEvent:FireServer("Third Drink")
