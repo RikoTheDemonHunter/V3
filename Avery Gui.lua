bf968388fac476b9efd829c3ac123c071ed9360f
Avery <126303785+RikoTheDemonHunter@users.noreply.github.com>
2025-06-29 09:27:20 +0800

    Update Avery Gui.lua

--- Avery Gui.lua
+++ Avery Gui.lua
@@ -1,3 +1,29 @@
+local whitelist = { "macmacinRoblox", "Rikothedev12", } 
+ 
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer -- 
+ 
+local function isWhitelisted(name)
+    for _, whitelistedName in ipairs(whitelist) do
+        if name == whitelistedName then
+            return true
+        end
+    end
+    return false
+end
+ 
+if not isWhitelisted(player.Name) then
+    game:GetService("StarterGui"):SetCore("SendNotification", {
+        Title = "Access Denied";
+        Text = "You are not whitelisted Dumbass!";
+        Duration = 5;
+    })
+    script.Parent:Destroy() -- 
+    return
+end
+ 
+print("Access granted to " .. player.Name)
+
 function AutoEquipDrink()
 	spawn(function()
 			if not game.Players.LocalPlayer.Backpack:FindFirstChild("Starter Drink") then
