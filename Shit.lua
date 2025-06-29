e664477fb9e1d7665620a34bdf7226e56e74c968
Avery <126303785+RikoTheDemonHunter@users.noreply.github.com>
2025-06-29 20:22:58 +0800

    Update Shit.lua

--- Shit.lua
+++ Shit.lua
@@ -1,3 +1,4 @@
+
 local HttpService = game:GetService("HttpService")
 local Players = game:GetService("Players")
 local player = Players.LocalPlayer
@@ -34,7 +35,6 @@ end
 -- If you get here, you’re allowed to execute the script
 -- Put your main script logic here:
 loadstring(game:HttpGet("https://raw.githubusercontent.com/RikoTheDemonHunter/Lua-Scripts/refs/heads/main/V3.lua"))()
-
 
 local lib = {}
 		
