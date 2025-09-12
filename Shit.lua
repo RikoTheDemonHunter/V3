5cec823b0fcca763457961f0f50b0fde2ad3c198
Avery <126303785+RikoTheDemonHunter@users.noreply.github.com>
2025-09-12 15:55:15 +0800

    Update Shit.lua

--- Shit.lua
+++ Shit.lua
@@ -948,6 +948,10 @@ Misc:Button("Plate", function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/RikoTheDemonHunter/V3/refs/heads/main/Plate.lua"))()
 end)
 
+Misc:Button("Castle", function()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/RikoTheDemonHunter/V3/refs/heads/main/Castle.lua"))()
+end)
+
 Misc:Button("Animation-Hub", function()
         loadstring(game:HttpGet("https://raw.githubusercontent.com/Gazer-Ha/Animated/main/G"))()
 end)	
