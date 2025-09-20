29b93ba21dbcf27f8e4a60bb5549fbca96b7919a
Avery <126303785+RikoTheDemonHunter@users.noreply.github.com>
2025-09-20 14:02:11 +0800

    Update Zero.lua

--- Zero.lua
+++ Zero.lua
@@ -1,5 +1,5 @@
 -- LOAD ORIONLIB
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local OrionLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/RikoTheDemonHunter/V3/refs/heads/main/Orion%20Library.lua"))()
 
 -- CREATE WINDOW
 local Window = OrionLib:MakeWindow({
