beaa4753f5a88d3064343c030d6f2b5b546e14f3
Avery <126303785+RikoTheDemonHunter@users.noreply.github.com>
2025-11-21 10:25:34 +0800

    Remove player whitelisting and access checks
    Removed whitelisting functionality and related notifications.

--- v2.lua
+++ v2.lua
@@ -84,33 +84,6 @@ Sign.Color = Color3.fromRGB(20, 200, 20)
 Sign.Material = Enum.Material.Metal
 Sign.Orientation = Vector3.new(0, -180, 0)
 
-
-local whitelist = { "macmacinRoblox", "Rikothedev12", "yurrgirlKaylee", } 
- 
-local Players = game:GetService("Players")
-local player = Players.LocalPlayer -- 
- 
-local function isWhitelisted(name)
-    for _, whitelistedName in ipairs(whitelist) do
-        if name == whitelistedName then
-            return true
-        end
-    end
-    return false
-end
- 
-if not isWhitelisted(player.Name) then
-    game:GetService("StarterGui"):SetCore("SendNotification", {
-        Title = "Access Denied";
-        Text = "You are not whitelisted Dumbass!";
-        Duration = 5;
-    })
-    script.Parent:Destroy() -- 
-    return
-end
- 
-print("Access granted to " .. player.Name)
-
 local lib = {}
 		
 function lib:Gui(title)
